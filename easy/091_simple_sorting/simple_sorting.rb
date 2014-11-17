bb238e8d847ee6d613d09d769bf77d58e036e7f6
rochefort <terasawan@gmail.com>
2014-11-18 07:31:20 +0900

    Fix simple sorting

--- easy/091_simple_sorting/simple_sorting.rb
+++ easy/091_simple_sorting/simple_sorting.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby -w
+# 
+# def selection_sort(numbers)
+#   tmp = numbers.dup
+#   res = []
+#   until tmp.empty?
+#     min_index = tmp.find_index(tmp.min)
+#     res << tmp.delete_at(min_index)
+#   end
+#   res
+# end
+# 
+# ARGF.each_line do |line|
+#   numbers = line.chomp.split.map(&:to_f)
+#   puts selection_sort(numbers).join(' ')
+# end
 
-def selection_sort(numbers)
-  tmp = numbers.dup
-  res = []
-  until tmp.empty?
-    min_index = tmp.find_index(tmp.min)
-    res << tmp.delete_at(min_index)
-  end
-  res
-end
-
+#!/usr/bin/env ruby -w
 ARGF.each_line do |line|
   numbers = line.chomp.split.map(&:to_f)
-  puts selection_sort(numbers).join(' ')
+  puts numbers.sort.map { |n| sprintf('%.3f', n) }.join(' ')
 end
